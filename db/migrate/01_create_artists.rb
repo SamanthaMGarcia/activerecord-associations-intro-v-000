3342baac11579b6b0de9ae0608986796a0df56a4
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-30 15:11:56 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,13 +1,3 @@
-class CreateSongs < ActiveRecord::Migration[4.2]
-  def change
-    create_table :songs do |t|
-      t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
-    end
-  end
-end
-
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists do |t|
